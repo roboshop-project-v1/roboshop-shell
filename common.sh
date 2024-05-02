7ed0848eefa35301d652f0e5c79985c0b4d3ff1f
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-03 00:02:40 +0530

    optimised the code

--- common.sh
+++ common.sh
@@ -1,9 +1,9 @@
 
 func_exit_status(){
     if [ $? -eq 0 ];then
-        echo   "\e[36mSUCCESS\e[0m"
+        echo -e "\e[36mSUCCESS\e[0m"
     else
-        echo  "\e[36mFAILURE\e[0m"
+        echo -e "\e[36mFAILURE\e[0m"
     fi
 }
 
